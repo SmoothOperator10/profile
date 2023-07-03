66830364dc75be617340d615e40a18718f65cb06
SmoothOperator10 <124905770+SmoothOperator10@users.noreply.github.com>
2023-07-03 17:19:58 +0100

    Add files via upload

---
+++ assets/js/functions.js
@@ -0,0 +1,55 @@
+function modalEIP() {
+  $("#modalEIPCont").modal("show");
+}
+
+function modalRE() {
+  $("#modalRECont").modal("show");
+}
+
+function modalLP() {
+  $("#modalLPCont").modal("show");
+}
+
+$(document).ready(function () {
+  $("#modalEIPCont").modal();
+    $("#modalRECont").modal();
+        $("#modalLPCont").modal();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
